32a3b741939defbb576c4bc324918746a6ebfd0d
Felix Alm <felix.kauppi.alm@gmail.com>
2017-10-05 15:32:31 +0200

    Removed unused imports

--- src/com/company/java/game/micro/Main.java
+++ src/com/company/java/game/micro/Main.java
@@ -1,10 +1,5 @@
 package com.company.java.game.micro;
 
-import com.googlecode.lanterna.*;
-import com.googlecode.lanterna.TerminalFacade;
-import com.googlecode.lanterna.input.Key;
-import com.googlecode.lanterna.terminal.Terminal;
-
 import javax.swing.*;
 import java.nio.charset.Charset;
 
@@ -20,6 +15,5 @@ public class Main {
         else {
             System.exit(0);
         }
-
     }
 }
